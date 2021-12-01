0c723895b42c1eadceb1d08acf0c7a0497bc5f2e
Fredrik Sidh <fredrik.sidh@hotmail.com>
2021-12-01 11:19:22 +0100

    Day 1 solution p2

--- 2021-1.py
+++ 2021-1.py
@@ -1,18 +1,34 @@
 import os
-filename = 'input_' + os.path.splitext(os.path.basename(__file__))[0] + '.txt'
-input_dir = os.path.join(os.path.dirname(__file__), 'input_2021', filename)
 
-with open(input_dir, 'r') as f:
-    input = f.readlines()
-    input = [int(line.strip()) for line in input]
+## PART A
+def countIncrease(input):
+    inc_count = 0
+    cur_measure = input[0]
 
-# Solution start
-inc_count = 0
-cur_measure = input[0]
+    for measure in input:
+        if measure > cur_measure:
+            inc_count += 1
+        cur_measure = measure
+    return inc_count
 
-for measure in input:
-    if measure > cur_measure:
-        inc_count += 1
-    cur_measure = measure
+## PART B
+def slidingAverage(input):
+    new_measures = []
+    for i in range(0,len(input)-2):
+        new_measure = sum(input[i:i+3])
+        new_measures.append(new_measure)
+    return new_measures
 
-print(inc_count)
+if __name__ == '__main__':
+    filename = 'input_' + os.path.splitext(os.path.basename(__file__))[0] + '.txt'
+    input_dir = os.path.join(os.path.dirname(__file__), 'input_2021', filename)
+    with open(input_dir, 'r') as f:
+        input = f.readlines()
+        input = [int(line.strip()) for line in input]
+    
+    # Part A
+    print('Ans part A:',countIncrease(input))
+    
+    # Part B
+    new_input = slidingAverage(input)
+    print('Ans part B:',countIncrease(new_input))
